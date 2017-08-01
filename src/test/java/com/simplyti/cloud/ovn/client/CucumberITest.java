3b4ce4d1b218d30ffe6f3572431c7259bacdf9c5
Pablo Táboas <pablo.taboas@bbva.com>
2017-08-01 02:07:15 +0200

    Add cucumber pretty plugin

--- src/test/java/com/simplyti/cloud/ovn/client/CucumberITest.java
+++ src/test/java/com/simplyti/cloud/ovn/client/CucumberITest.java
@@ -9,5 +9,6 @@ import cucumber.api.junit.Cucumber;
 @RunWith(Cucumber.class)
 @CucumberOptions(
 		features="classpath:features",
-		snippets=SnippetType.CAMELCASE)
+		snippets=SnippetType.CAMELCASE,
+		plugin="pretty")
 public class CucumberITest {}
