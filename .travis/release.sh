ff5dc087b55d58960bb523d8e6be956a203dc3b2
Pablo Táboas <pablo.taboas@bbva.com>
2017-10-11 01:43:11 +0200

    remove v* prefix from version tag

--- .travis/release.sh
+++ .travis/release.sh
@@ -3,6 +3,6 @@
 openssl aes-256-cbc -K $encrypted_fd725cdc679b_key -iv $encrypted_fd725cdc679b_iv -in ./.travis/codesigning.asc.enc -out codesigning.asc -d
 gpg --fast-import codesigning.asc
 
-version=${TRAVIS_TAG%v*}
-echo "Release version $version"
-mvn versions:set -DnewVersion=$version -DgenerateBackupPoms=false
+VERSION=$(echo "$TRAVIS_TAG" | sed 's/^v//g')
+echo "Release version $VERSION"
+mvn versions:set -DnewVersion=$VERSION -DgenerateBackupPoms=false
