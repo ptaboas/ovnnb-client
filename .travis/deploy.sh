38acfb4a58f55ff35857e3d4f91fd4b2e7e46062
Pablo Táboas <pablo.taboas@bbva.com>
2017-10-30 21:07:39 +0100

    Upgrade snapshot version Fix build scripts

--- .travis/deploy.sh
+++ .travis/deploy.sh
@@ -3,4 +3,4 @@
 openssl aes-256-cbc -K $encrypted_fd725cdc679b_key -iv $encrypted_fd725cdc679b_iv -in ./.travis/codesigning.asc.enc -out codesigning.asc -d
 gpg --fast-import codesigning.asc
 
-mvn --settings=./.travis/settings.xml jar:jar source:jar-no-fork javadoc:jar gpg:sign deploy:deploy
+mvn --settings=./.travis/settings.xml compile jar:jar source:jar-no-fork javadoc:jar gpg:sign deploy:deploy
--- .travis/release.sh
+++ .travis/release.sh
@@ -1,9 +1,10 @@
 #!/usr/bin/env bash
 
-openssl aes-256-cbc -K $encrypted_fd725cdc679b_key -iv $encrypted_fd725cdc679b_iv -in ./.travis/codesigning.asc.enc -out codesigning.asc -d
-gpg --fast-import codesigning.asc
-
 VERSION=$(echo "$TRAVIS_TAG" | sed 's/^v//g')
 echo "Release version $VERSION"
 mvn versions:set -DnewVersion=$VERSION -DgenerateBackupPoms=false
-mvn --settings=./.travis/settings.xml jar:jar source:jar-no-fork javadoc:jar gpg:sign deploy:deploy
+
+openssl aes-256-cbc -K $encrypted_fd725cdc679b_key -iv $encrypted_fd725cdc679b_iv -in ./.travis/codesigning.asc.enc -out codesigning.asc -d
+gpg --fast-import codesigning.asc
+
+mvn --settings=./.travis/settings.xml compile jar:jar source:jar-no-fork javadoc:jar gpg:sign deploy:deploy
